8e270676d96130f0a5131ec6a3c4e05377fa7d50
Tony Wildish <Tony.Wildish@ebi.ac.uk>
2015-10-26 16:02:03 +0000

    stripped to the bone

--- perl_lib/Lifecycle/Core/Config/Agent.pm
+++ perl_lib/Lifecycle/Core/Config/Agent.pm
@@ -1,76 +1,53 @@
 package Lifecycle::Core::Config::Agent;
+# use File::Basename;
 
-use File::Basename;
-our %params = (
-		LABEL     => undef,
-		# PROGRAM   => undef,
-		DEFAULT   => 'on',
-		# ENVIRON   => 'common',
-		# OPTIONS   => undef,
-		# STATELINK => undef,
-		# STATEOPT  => undef,
-		# NICE      => undef,
-		# OPTS      => undef,
-	      );
+# our %params = (
+# 	LABEL     => undef,
+# 	DEFAULT   => 'on',
+# );
 
-sub new
-{
-  my $proto = shift;
-  my $class = ref($proto) || $proto;
-  my $self = {};
-  $self  = ref($proto) ? $class->SUPER::new(@_) : {};
-
-  my %args = (@_);
-  defined($args{LABEL}) or die "Unnamed Agents are not allowed\n";
-
-  map {
-        $self->{$_} = defined($args{$_}) ? $args{$_} : $params{$_}
-      } keys %params;
-
-  bless $self, $class;
-  return $self;
-}
-
-# sub AUTOLOAD
+# sub new
 # {
-#   my $self = shift;
-#   my $attr = our $AUTOLOAD;
-#   $attr =~ s/.*:://;
-#   if ( exists($params{$attr}) )
-#   {
-#     $self->{$attr} = shift if @_;
-#     return $self->{$attr};
-#   }
+#   my $proto = shift;
+#   my $class = ref($proto) || $proto;
+#   my $self = {};
+#   $self  = ref($proto) ? $class->SUPER::new(@_) : {};
 
-#   return unless $attr =~ /[^A-Z]/;  # skip DESTROY and all-cap methods
-#   my $parent = "SUPER::" . $attr;
-#   $self->$parent(@_);
+#   # my %args = (@_);
+#   # defined($args{LABEL}) or die "Unnamed Agents are not allowed\n";
+
+#   # map {
+#   #       $self->{$_} = defined($args{$_}) ? $args{$_} : $params{$_}
+#   #     } keys %params;
+
+#   bless $self, $class;
+#   return $self;
 # }
 
-sub LOGDIR
-{
-  my ($self,$dir) = @_;
-  $self->{LOGDIR} = $dir ||
-		      $self->{LOGDIR} ||
-		      "\${PHEDEX_LOGS}/";
-  $self->{LOGDIR} .= '/' unless $self->{LOGDIR} =~ m%\/$%;
-  return $self->{LOGDIR};
-}
+# sub LOGDIR
+# {
+#   my ($self,$dir) = @_;
+#   $self->{LOGDIR} = $dir ||
+# 		      $self->{LOGDIR} ||
+# 		      "\${PHEDEX_LOGS}/";
+#   $self->{LOGDIR} .= '/' unless $self->{LOGDIR} =~ m%\/$%;
+#   return $self->{LOGDIR};
+# }
 
-sub LOGFILE
-{
-  my ($self,$file) = @_;
-  die "Cannot set LOGFILE, can only set LOGDIR\n" if $file;
-  $self->{LOGFILE} = $self->LOGDIR . $self->{LABEL};
-  return $self->{LOGFILE};
-}
+# sub LOGFILE
+# {
+#   my ($self,$file) = @_;
+#   die "Cannot set LOGFILE, can only set LOGDIR\n" if $file;
+#   $self->{LOGFILE} = $self->LOGDIR . $self->{LABEL};
+#   return $self->{LOGFILE};
+# }
 
-sub PIDFILE
-{
-  my ($self,$file) = @_;
-  $self->{PIDFILE} = $$file if $file;
-  $self->{PIDFILE} = $self->DROPDIR . 'pid' unless $self->{PIDFILE};
-  return $self->{PIDFILE};
-}
+# sub PIDFILE
+# {
+#   my ($self,$file) = @_;
+#   $self->{PIDFILE} = $$file if $file;
+#   $self->{PIDFILE} = $self->DROPDIR . 'pid' unless $self->{PIDFILE};
+#   return $self->{PIDFILE};
+# }
 
 1;
