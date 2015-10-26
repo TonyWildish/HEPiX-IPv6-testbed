2da3ee020b5d168c3556df03c7edf913a1e71bbf
Tony Wildish <Tony.Wildish@ebi.ac.uk>
2015-10-26 12:52:18 +0000

    more cleanup

--- perl_lib/Lifecycle/Core/Agent.pm
+++ perl_lib/Lifecycle/Core/Agent.pm
@@ -1,11 +1,5 @@
 package Lifecycle::Core::Agent;
 
-=head1 NAME
-
-Lifecycle::Core::Agent - POE-based Agent daemon base class
-
-=cut
-
 use strict;
 use warnings;
 use base 'Lifecycle::Core::Logging';
@@ -19,65 +13,27 @@ use Lifecycle::Core::Config;
 use Lifecycle::Core::Loader;
 use Data::Dumper;
 
-# %params, %args, config-files...?
-# Precedence is: command-line(%args), config-files, %params(default)
-# but %params is the definitive source of the list of legal keys, so all keys
-# for the agent should be listed here.
-
-=head1 Agent initialisation
-
-Agents can be initialised with default parameters, with parameters from 
-Config files, or with command-line arguments. That's also the order of 
-precedence, config files override defaults and command-line arguments 
-override config files.
-
-Defaults are defined in the %params hash in the agent 
-module. The Lifecycle::Core::Agent module has defaults for most of the 
-parameters you will ever need, if you have more or different default 
-values you need only define them in your own %params hash in your own 
-agent.
-
-Getting the precedence-order for setting parameters is not trivial. For
-simple scalars it's easy, you need only check if the value is defined on
-the command-line, the config file, and the %params hash, and set it from
-the first one that defines it. Note that you need to check for defined,
-not 'true', in case the default is 0 or false.
-
-For array or hash values, such as IGNORE_NODES, it's more complex. You
-can't put a Perl array or hash into a config file, or on the command-line,
-but you can put a comma-separated list there. So, the technique is to 
-leave the value of such parameters undef in the %params hash and declare 
-separately two arrays, @array_params and @hash_params, which hold the 
-key-names of the array and hash parameters. When the object is initialised 
-(via the C<< init >> method, at the beginning of the C<< process >> method)
-the default C<< init >> method will check which parameters are required to be
-arrays or hashes and either set them to null arrays/hashes if they are not defined 
-or, if they have scalar values, will split the scalar on commas and set 
-them from that.
-
-=cut
-
 our %params =
 	(
 	  ME		=> undef,
 	  DBH		=> undef,
-	  SHARED_DBH	=> 0,
-	  DBCONFIG	=> undef,
-	  DROPDIR	=> undef,
-	  NEXTDIR	=> undef,
-	  INBOX		=> undef,
-	  WORKDIR	=> undef,
-	  OUTBOX	=> undef,
+	  # SHARED_DBH	=> 0,
+	  # DBCONFIG	=> undef,
+	  # DROPDIR	=> undef,
+	  # NEXTDIR	=> undef,
+	  # INBOX		=> undef,
+	  # WORKDIR	=> undef,
+	  # OUTBOX	=> undef,
 	  STOPFILE	=> undef,
 	  PIDFILE	=> undef,
 	  LOGFILE	=> undef,
-	  NODES		=> undef,
-	  IGNORE_NODES	=> undef,
-	  ACCEPT_NODES	=> undef,
+	  # NODES		=> undef,
+	  # IGNORE_NODES	=> undef,
+	  # ACCEPT_NODES	=> undef,
 	  WAITTIME	=> 7,
-	  AUTO_NAP      => 1,
-	  JUNK		=> undef,
-	  BAD		=> undef,
+	  # AUTO_NAP      => 1,
+	  # JUNK		=> undef,
+	  # BAD		=> undef,
 	  STARTTIME	=> undef,
 	  NWORKERS	=> 0,
 	  WORKERS	=> undef,
@@ -87,21 +43,19 @@ our %params =
 	  AGENT		=> undef,
 	  DEBUG         => $ENV{PHEDEX_DEBUG} || 0,
  	  VERBOSE       => $ENV{PHEDEX_VERBOSE} || 0,
-	  NOTIFICATION_HOST   => undef,
-	  NOTIFICATION_PORT   => undef,
-	  _DOINGSOMETHING     => 0,
-	  _DONTSTOPME	      => 0,
-	  STATISTICS_INTERVAL => 3600,	# reporting frequency
-	  STATISTICS_DETAIL   => 1,	# reporting level: 0, 1, or 2
-          LOAD_DROPBOX        => 1,     # Load Dropbox module...
-          LOAD_DROPBOX_WORKDIRS => 0,   # ...but not all the directories...
-          LOAD_CYCLE          => 1,     # Load Cycle module
-          LOAD_DB             => 1,     # Load DB module
+	  # NOTIFICATION_HOST   => undef,
+	  # NOTIFICATION_PORT   => undef,
+	  # _DOINGSOMETHING     => 0,
+	  # _DONTSTOPME	      => 0,
+	  # STATISTICS_INTERVAL => 3600,	# reporting frequency
+	  # STATISTICS_DETAIL   => 1,	# reporting level: 0, 1, or 2
+          # LOAD_DROPBOX        => 1,     # Load Dropbox module...
+          # LOAD_DROPBOX_WORKDIRS => 0,   # ...but not all the directories...
+          # LOAD_CYCLE          => 1,     # Load Cycle module
+          # LOAD_DB             => 1,     # Load DB module
 	);
 
-our (@array_params,@hash_params,@required_params,@writeable_dirs,@writeable_files);
-@array_params = qw / STARTTIME /;
-@hash_params  = qw / BAD JUNK /;
+our (@required_params,@writeable_dirs,@writeable_files);
 sub new
 {
     my $proto = shift;
@@ -112,8 +66,6 @@ sub new
     my %args = (@_);
     my $me = $self->AgentType($args{ME});
 
-#   variable to hold plugin modules. Note they are local, we already have make sure that
-#   in case of a modules is loaded the given class is store 
     my @agent_module_reject = ( qw / Template Agent AgentLite/ );
     my $agent_module_loader = Lifecycle::Core::Loader->new( NAMESPACE => 'Lifecycle',
                                                             REJECT    => \@agent_module_reject );
@@ -199,13 +151,10 @@ sub new
       my $dropbox_module = $agent_module_loader->Load('Dropbox')->new( $self );
     }
 
-#   Basic validation: Explicitly call the base method to validate only the
-#   core agent. This may be called again in the derived agent, to validate
-#   other parameters. No harm in that!
-    die "$me: Failed validation, exiting\n" if $self->isInvalid();
-
-#   Clean PID and STOP flags. This method is always defined.
-    $self->cleanDropbox($me);
+    if ( !defined($self->{LOGFILE}) && !$self->{NODAEMON} )
+    {
+      $self->Fatal("PERL_FATAL: LOGFILE not set but process will run as a daemon");
+    }
 
 #   If required, daemonise, write pid file and redirect output.
     $self->daemon();
@@ -213,115 +162,37 @@ sub new
 #   Load the Cycle modules. _Cycle subclass is create and attach to self
     my $cycle_module = $agent_module_loader->Load('Cycle')->new( $self ) if $self->{LOAD_CYCLE};
      
-#   Initialise subclass.
-    $self->init();
-
-#   Load the DB modules. _DB subclass is create and attach to self
-    if ( $self->{LOAD_DB} ) {
-      my $db_module = $agent_module_loader->Load('DB')->new( $self );
-    }
-
-#   Validate the object!. This method is always defined.
-    die "Agent ",$self->{ME}," failed validation\n" if $self->isInvalid();
-
 #   Announce myself...
     $self->Logmsg("label=$label");
-    $self->Dbgmsg("Agent was loaded DB=>$self->{LOAD_DB}, CYCLE=$self->{LOAD_CYCLE}, DROPBOX=$self->{LOAD_DROPBOX}") if $self->{DEBUG};
 
     bless $self, $class;
     return $self;
 }
 
-sub isInvalid {
-  my $self = shift;
-  my %h = @_;
-  @{$h{REQUIRED}} = @required_params unless $h{REQUIRED};
-  @{$h{WRITEABLE_DIRS}}  = @writeable_dirs  unless $h{WRITEABLE_DIRS};
-  @{$h{WRITEABLE_FILES}} = @writeable_files unless $h{WRITEABLE_FILES};
-
-  my $errors = 0;
-  foreach ( @{$h{REQUIRED}} )
-  {
-   next if defined $self->{$_};
-    $errors++;
-    $self->Warn("Required parameter \"$_\" not defined!\n");
-  }
-
-# Some parameters must be writeable directories
-  foreach my $key ( @{$h{WRITEABLE_DIRS}} )
-  {
-    $_ = $self->{$key};
-    while ( my $x = readlink($_) ) { $_ = $x; } # Follow symlinks!
-
-#   If the directory doesn't exist, attempt to create it...
-    eval { mkpath $_ } unless -e;
-    $self->Fatal("PERL_FATAL: $key directory $_ does not exist")   unless -e;
-    $self->Fatal("PERL_FATAL: $key exists but is not a directory") unless -d;
-    $self->Fatal("PERL_FATAL: $key directory $_ is not writeable") unless -w;
-  }
-
-# Some parameters must be writeable files if they exist, or the parent
-# directory must be writeable. Non-definition is tacitly allowed
-  foreach my $key ( @{$h{WRITEABLE_FILES}} )
-  {
-    if ( defined($_=$self->{$key}) )
-    {
-      while ( my $x = readlink($_) ) { $_ = $x; } # Follow symlinks!
-      if ( -e $_ )
-      {
-#       If it exists, it better be a writeable file
-        $self->Fatal("PERL_FATAL: $key exists but is not a file") unless -f;
-        $self->Fatal("PERL_FATAL: $key file $_ is not writeable") unless -w;
-      }
-      else
-      {
-#       If it doesn't exist, the parent must be a writeable directory
-#       If that parent directory doesn't exist, attempt to create it...
-        if ( ! -e )
-        {
-          $_ = dirname($_);
-          eval { mkpath $_ } unless -e;
-        }
-        $self->Fatal("PERL_FATAL: $key directory $_ does not exist")   unless -e;
-        $self->Fatal("PERL_FATAL: $key exists but is not a directory") unless -d;
-        $self->Fatal("PERL_FATAL: $key directory $_ is not writeable") unless -w;
-      }
-    }
-  }
-
-  if ( !defined($self->{LOGFILE}) && !$self->{NODAEMON} )
-  {
-#   LOGFILE not defined is fatal unless NODAEMON is set!
-    $self->Fatal("PERL_FATAL: LOGFILE not set but process will run as a daemon");
-  }
-
-  return $errors;
-}
-
 # Dummy functions for Dropbox module
-sub readInbox {}
-sub readPending {}
-sub readOutbox {}
-sub renameDrop {}
-sub inspectDrop {}
-sub markBad {}
-sub processInbox {}
-sub processOutbox {}
-sub processWork {}
-sub processIdle {}
-sub cleanDropbox { }
+# sub readInbox {}
+# sub readPending {}
+# sub readOutbox {}
+# sub renameDrop {}
+# sub inspectDrop {}
+# sub markBad {}
+# sub processInbox {}
+# sub processOutbox {}
+# sub processWork {}
+# sub processIdle {}
+# sub cleanDropbox { }
 
 # Dummy functions for DB module
-sub connectAgent {}
-sub disconnectAgent {}
-sub rollbackOnError {}
-sub checkNodes {}
-sub identifyAgent {}
-sub updateAgentStatus {}
-sub checkAgentMessages {}
-sub expandNodes {}
-sub myNodeFilter {}
-sub otherNodeFilter {}
+# sub connectAgent {}
+# sub disconnectAgent {}
+# sub rollbackOnError {}
+# sub checkNodes {}
+# sub identifyAgent {}
+# sub updateAgentStatus {}
+# sub checkAgentMessages {}
+# sub expandNodes {}
+# sub myNodeFilter {}
+# sub otherNodeFilter {}
 
 # Dummy functions for Cycle module
 sub preprocess {}
@@ -351,17 +222,6 @@ sub maybeStop
 
 sub doExit{ my ($self,$rc) = @_; exit($rc); }
 
-=head1 Running agents as daemons
-
-Agents will, be default, become daemons by forking, disconnecting from the
-terminal, and starting their own process group. If you're trying to debug
-them this can be a bit of a problem, so you can turn off this behaviour by
-passing the NODAEMON flag with a non-zero value to the agents constructor.
-
-=cut
-
-# Turn the process into a daemon.  This causes the process to lose
-# any controlling terminal by forking into background.
 sub daemon
 {
     my ($self, $me) = @_;
@@ -376,24 +236,24 @@ sub daemon
 #     I may not be a daemon, but I still have to write the PIDFILE, or the
 #     watchdog may start another incarnation of me!
       ((print PIDFILE "$$\n") && close(PIDFILE))
-	or die "$me: fatal error: cannot write to PID file ($self->{PIDFILE}): $!\n";
+      	or die "$me: fatal error: cannot write to PID file ($self->{PIDFILE}): $!\n";
       close PIDFILE;
       return;
     }
 
     # Fork once to go to background
     die "failed to fork into background: $!\n"
-	if ! defined ($pid = fork());
+  	if ! defined ($pid = fork());
     close STDERR if $pid; # Hack to suppress misleading POE kernel warning
     exit(0) if $pid;
 
     # Make a new session
     die "failed to set session id: $!\n"
-	if ! defined setsid();
+	  if ! defined setsid();
 
     # Fork another time to avoid reacquiring a controlling terminal
     die "failed to fork into background: $!\n"
-	if ! defined ($pid = fork());
+  	if ! defined ($pid = fork());
     close STDERR if $pid; # Hack to suppress misleading POE kernel warning
     exit(0) if $pid;
 
@@ -402,7 +262,7 @@ sub daemon
 
     # Write our pid to the pid file while we still have the output.
     ((print PIDFILE "$$\n") && close(PIDFILE))
-	or die "$me: fatal error: cannot write to $self->{PIDFILE}: $!\n";
+	  or die "$me: fatal error: cannot write to $self->{PIDFILE}: $!\n";
 
     # Indicate we've started
     print "$me: pid $$", ( $self->{DROPDIR} ? " started in $self->{DROPDIR}" : '' ), "\n";
@@ -410,68 +270,11 @@ sub daemon
     # Close/redirect file descriptors
     $self->{LOGFILE} = "/dev/null" if ! defined $self->{LOGFILE};
     open (STDOUT, ">> $self->{LOGFILE}")
-	or die "$me: cannot redirect output to $self->{LOGFILE}: $!\n";
+  	or die "$me: cannot redirect output to $self->{LOGFILE}: $!\n";
     open (STDERR, ">&STDOUT")
-	or die "Can't dup STDOUT: $!";
+  	or die "Can't dup STDOUT: $!";
     open (STDIN, "</dev/null");
     $|=1; # Flush output line-by-line
-}
-
-=head1 User hooks
-
-There are a number of user-hooks for overriding base-class behaviour at
-various points. If you choose to you them you should check what the base-class
-method does, and insert a call to C<< $self->SUPER::method >> somewhere
-appropriate, if needed.
-
-=head2 init
-
-Called from C<< process, >> this method makes sure that all parameters that are
-supposed to be hashes or arrays are correctly initialised. See the description
-of initialising agents, above, for details.
-
-You should not normally need to override the default init method, but if you
-do you should call this base method early in the overridden code, via
-C<< $self->SUPER::init >>.
-
-Unlike C<< isInvalid >>, C<< init >> does have side-effects, so should not be
-called at random in your code. The advantage of having an C<< init >> method,
-instead of doing everything in the constructor, is that you can defer some
-checks until you intend to use the object. This will be useful when/if we get
-round to running more than one agent in the same process, for example.
-
-=cut
-
-sub init
-{
-  my $self = shift;
-  my %h = @_;
-  @{$h{ARRAYS}} = @array_params unless $h{ARRAYS};
-  @{$h{HASHES}} = @hash_params  unless $h{HASHES};
-  foreach ( @{$h{ARRAYS}} )
-  {
-    if ( !defined($self->{$_}) )
-    {
-      $self->{$_} = [];
-      next;
-    }
-    next if ref($self->{$_}) eq 'ARRAY';
-    my @x = split(',',$self->{$_});
-    $self->{$_} = \@x;
-  }
-
-  foreach ( @{$h{HASHES}} )
-  {
-    if ( !defined($self->{$_}) )
-    {
-      $self->{$_} = {};
-      next;
-    }
-#   Is this the right thing to do here...?
-    next if ref($self->{$_}) eq 'HASH';
-    my %x = split(',',$self->{$_});
-    $self->{$_} = \%x;
-  }
 }
 
 # Actually make the agent stop and exit.
@@ -481,12 +284,6 @@ sub doStop
 
     # Run agent cleanup
     eval { $self->stop(); };
-    $self->rollbackOnError();
-
-    # Force database off
-    eval { $self->{DBH}->rollback() } if $self->{DBH};
-    eval { &disconnectFromDatabase($self, $self->{DBH}, 1) } if $self->{DBH};
-
     # Remove stop flag and pidfile
     unlink($self->{PIDFILE});
     #unlink($self->{STOPFILE});
@@ -495,38 +292,20 @@ sub doStop
     $self->doExit(0);
 }
 
-
-=head2 stop
-
-Agent subclasses implement this in order to do any final clean up
-actions before exiting.  The actions should finish promptly.
-
-=cut
-
 sub stop {}
-
-=head2 processDrop
-
-Override this in an agent subclass to process a drop file
-
-=cut
-
 sub processDrop {}
-
-# Manage work queue.  If there are previously pending work, finish
-# it, otherwise look for and process new inbox drops.
 
 sub process
 {
   my $self = shift;
+# TW
+die "Don't want this, do I?\n";
   # Work.
 
-  my $pmon = $self->{pmon};
   $self->processInbox();
   my @pending = $self->processWork();
   $self->processOutbox();
   $self->processIdle(@pending);
-  $self->Dbgmsg($pmon->FormatStates) if $self->{DEBUG};
   # Check to see if the config-file should be reloaded
   $self->checkConfigFile();
 }
--- perl_lib/Lifecycle/Core/Config/Environment.pm
+++ perl_lib/Lifecycle/Core/Config/Environment.pm
@@ -1,94 +1,5 @@
 package Lifecycle::Core::Config::Environment;
 
-=head1 NAME
-
-Lifecycle::Core::Config::Environment - Manage Agent environment configuration parameters
-
-=head1 SYNOPSIS
-
-This module is used by L<Lifecycle::Core::Config|Lifecycle::Core::Config> to expand
-environment variable definitions for anyone that needs them. There are no
-methods for setting individual variables, only for inspecting them or for
-setting the entire environment.
-
-=head1 DESCRIPTION
-
-Lifecycle::Core::Config::Environment objects are created and used by the
-L<Lifecycle::Core::Config|Lifecycle::Core::Config> module. They allow an agent or
-utility to inspect the environment used for a given agent, with the possibility
-of fully expanding the definitions to final pathnames etc.
-
-Environments can be hierarchical, the default being that all environments have
-the "common" environment as a parent. So a parameter that is not defined in
-a particular environment may be defined in its parent, and so on.
-
-The environments themselves do not manage their parentage, it is up to the
-L<Lifecycle::Core::Config|Lifecycle::Core::Config> object that creates them to set
-the hierarchy by calling the PARENT method with the name of the parent
-environment.
-
-=head1 METHODS
-
-=over
-
-=item Environment( $string )
-
-Set or return a string containing the full environment definition, with parents
-at the head, but B<not> the environment of the actual script as well. I.e. it
-only returns values that were read from the configuration file, but nothing that
-already exists in the environment the process was executed in. This can be fed
-straight to the shell to execute, to actually instantiate the environment.
-
-=item getParameter( $string )
-
-Takes a string parameter-name as input, and returns the value of that parameter
-from the environment. If the parameter is not defined in the immediate
-environment, it will be sought in the parent environments, until it is found.
-If it is not found anywhere in the hierarchy of environments, it is sought in
-the environment that the script is running in.
-
-=item getExpandedParameter( $string )
-
-Like C<getParameter>, but recursively expands variables used in the definition
-of the parameter. So C<$PHEDEX_PATH/file.txt> would have the C<$PHEDEX_PATH>
-expanded to its final value, etc. Recognises variables for expansion in the
-formats C<$VAR> or C<${VAR}> only.
-
-=item getExpandedString( $string )
-
-Like C<getExpandedParameter>, but expands the input string directly, instead of
-looking it up in the environment first. This allows you to evaluate arbitrary
-expansions using the environment variables.
-
-=item NAME( $string )
-
-Name of the environment, obligatory, must be set in the constructor.
-
-=item CONFIG( \$Lifecycle::Core::Config )
-
-The Config object that creates this Environment object. This is
-obligatory if you use hierarchies of environments, through the PARENT method.
-
-=item PARENT( $string )
-
-Set or get the name of the parent environment.
-
-=back
-
-=head1 EXAMPLES
-
-  my $config = Lifecycle::Core::Config->new();
-  $config->readConfig($file);
-  my $env = $config->{ENVIRONMENTS}{common};
-  print $env->getExpandedParameter('PHEDEX_MAP'),"\n";
-  print $env->getExpandedParameter('PERL5LIB'),"\n";
-  print $env->getExpandedParameter('WWW_HOME'),"\n";
-  my $a = $config->select_agents('info-fs');
-  print $env->getExpandedString($a->DROPDIR),"\n";
-  print $env->getExpandedString($a->OPTIONS->{-db}),"\n";
-
-=cut
-
 our %params = (
 		NAME	    => undef,
 		PARENT      => undef,
@@ -113,25 +24,27 @@ sub new
   return $self;
 }
 
-sub AUTOLOAD
-{
-  my $self = shift;
-  my $attr = our $AUTOLOAD;
-  $attr =~ s/.*:://;
-  if ( exists($params{$attr}) )
-  {
-    $self->{$attr} = shift if @_;
-    return $self->{$attr};
-  }
+# sub AUTOLOAD
+# {
+#   my $self = shift;
+#   my $attr = our $AUTOLOAD;
+#   $attr =~ s/.*:://;
+#   if ( exists($params{$attr}) )
+#   {
+#     $self->{$attr} = shift if @_;
+#     return $self->{$attr};
+#   }
 
-  return unless $attr =~ /[^A-Z]/;  # skip DESTROY and all-cap methods
-  my $parent = "SUPER::" . $attr;
-  $self->$parent(@_);
-}
+#   return unless $attr =~ /[^A-Z]/;  # skip DESTROY and all-cap methods
+#   my $parent = "SUPER::" . $attr;
+#   $self->$parent(@_);
+# }
 
 sub getParameter
 {
   my ($self,$parameter) = @_;
+# TW
+die "Getting parameter: $parameter\n";
   return undef unless defined $parameter;
 
   if ( ! defined($self->{KEYS}) )
@@ -155,6 +68,8 @@ sub getParameter
 sub getExpandedString
 {
   my ($self,$string) = @_;
+# TW
+die "Getting expanded string: $string\n";
   return undef unless defined $string;
 
   my $done = 0;
@@ -179,6 +94,8 @@ sub getExpandedString
 sub getExpandedParameter
 {
   my ($self,$parameter) = @_;
+# TW
+die print "Getting expanded parameter: $parameter\n";
   return undef unless defined $parameter;
 
   my $value = $self->getParameter($parameter);
