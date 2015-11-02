443a7971a70a0a1f24373574226f8b436c2f7abf
Tony Wildish <Tony.Wildish@ebi.ac.uk>
2015-11-02 14:38:44 +0000

    use /dev/urandom instead of /dev/random, which is too slow on Embassy VMs

--- env.sh
+++ env.sh
@@ -13,7 +13,7 @@ if [ ! -f data/file-100.dat ] || [ ! -f data/file-1000.dat ]; then
 
 # a 1 MB seed file
 	if [ ! -f data/file-1.dat ]; then
-		dd if=/dev/random of=data/file-1.dat bs=1024 count=1024 >/dev/null 2>&1
+		dd if=/dev/urandom of=data/file-1.dat bs=1024 count=1024 >/dev/null 2>&1
 	fi
 
 # a 100 MB file...
