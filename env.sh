24aa6043d95b0f70187f215583f1842a8252899c
Tony Wildish <Tony@Wildish.eu>
2015-11-24 13:28:55 +0000

    allow for spaces in directory names by enclosing environment variable values in double-quotes

--- env.sh
+++ env.sh
@@ -1,5 +1,6 @@
 export TESTBED_ROOT=`pwd`
-if [ ! -d $TESTBED_ROOT/EBI ] && [ ! -d $TESTBED_ROOT/HEPiX ]; then
+echo $TESTBED_ROOT
+if [ ! -d "$TESTBED_ROOT/EBI" ] && [ ! -d "$TESTBED_ROOT/HEPiX" ]; then
   echo "Looks like you're not in the root directory of the git repository...?"
   exit 0
 fi
